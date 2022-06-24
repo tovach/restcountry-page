7be6dfbb6dfbcf1f28f2e7f8198c9269f513e8e7
tovach <red5.dalv@gmail.com>
2022-06-24 11:04:17 +0300

    feat: finish project

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,17 @@
-const form = document.querySelector(".form");
-const clearListBtn = document.querySelector(".top__clear-list");
+//Global constants
+let searchedCountries = [];
 
+//Dom elements
+const form = document.querySelector(".form");
+const formInput = document.querySelector(".form__input");
+const clearListBtn = document.querySelector(".top__clear-list");
 const list = document.querySelector(".list");
+const popUp = document.querySelector(".pop-up");
 
 //http client
 const getData = async (url) => {
   let data;
+  let errorMessage;
   let errorCode;
   let status = false;
 
@@ -20,25 +26,27 @@ const getData = async (url) => {
       throw new Error(`${response.status} - ${response.statusText}`);
     }
   } catch (error) {
+    errorMessage = error;
     console.error(error);
   }
-  return { data, status, errorCode };
+  return { data, status, errorMessage, errorCode };
 };
 
 //get country info
 const getCountry = async (countryName) => {
   const url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-  const { data, errorCode, status } = await getData(url);
+  const { data, status, errorMessage, errorCode } = await getData(url);
   if (status) {
-    let [country] = data;
-    let {
+    const [country] = data;
+    const {
       flags: { svg: flagIcon },
       name: { common: commonName },
-      capital: [capital],
       currencies,
       population,
       borders,
     } = country;
+
+    const capital = country.capital || "?";
     setCountry(
       flagIcon,
       commonName,
@@ -48,6 +56,12 @@ const getCountry = async (countryName) => {
       population,
       borders
     );
+  } else {
+    if (errorCode) {
+      popUpToggler("Error! Check country full name");
+    } else {
+      popUpToggler(errorMessage.message);
+    }
   }
 };
 
@@ -78,18 +92,9 @@ const setCountry = (
                   </p>
                   <span class="country__population">${population}</span>
                 </div>
-                <div class="country__neighbors neighbors">
+                <div class="country__neighbors neighbors" data-borders="${borders}">
                   <h4 class="neighbors__heading">Neighbors</h4>
-                  <ul class="neighbors__list">
-                    ${borders.map((neighbor) => {
-                      return `<li class="neighbors__item">
-                      <div class="neighbors__flag">
-                        <img src="https://flagcdn.com/se.svg">
-                      </div>
-                      <h5 class="neighbors__name">${neighbor}</h5>
-                    </li>`;
-                    }).join('\n')}
-                  </ul>
+                  <ul class="neighbors__list"></ul>
                 </div>
             </article>
        </li>
@@ -102,41 +107,99 @@ const setCountry = (
 const setCountriesListener = () => {
   const countries = document.querySelectorAll(".country");
   countries.forEach((country) =>
-    country.removeEventListener("click", setNeighbor)
+    country.removeEventListener("click", countryClickHandler)
   );
   countries.forEach((country) =>
-    country.addEventListener("click", setNeighbor)
+    country.addEventListener("click", countryClickHandler)
   );
 };
 
 //get neighbor
-const getNeighbor = async (countryCode) => {
-  const url = `https://restcountries.com/v3.1/name/${countryCode}`;
+const getNeighbor = async (countryCode, clickedCountry) => {
+  const url = `https://restcountries.com/v3.1/alpha/${countryCode}`;
   const { data, errorCode, status } = await getData(url);
   if (status) {
-    let [country] = data;
-    let {
+    const [country] = data;
+    const {
       flags: { svg: flagIcon },
       name: { common: commonName },
-      capital: [capital],
+      cca3,
+      // capital: [capital],
       currencies,
       population,
+      borders,
     } = country;
+
+    const capital = country.capital || "?";
+    const neighborObj = {
+      flagIcon,
+      commonName,
+      cca3,
+      capital,
+      currencies,
+      population,
+      borders,
+    };
+
+    setNeighbor(neighborObj, clickedCountry);
+  } else {
+    popUpToggler("Error! Neighbors ?");
   }
 };
 
 //set neighbor
-const setNeighbor = (e, obj) => {
-  const height = e.currentTarget.lastElementChild.clientHeight;
-  e.currentTarget.lastElementChild.classList.toggle('active');
-  e.currentTarget.lastElementChild.style.bottom = `-${height+10}px`;
-  
-  console.log(e.currentTarget);
-  // if (e.currentTarget.lastElementChild.classList.contains("neighbors")) return;
+const setNeighbor = (neighborObj, clickedCountry) => {
+  const {
+    flagIcon,
+    commonName,
+    cca3,
+    capital,
+    currencies,
+    population,
+    borders,
+  } = neighborObj;
+
+  const neighbors = clickedCountry.querySelector(".neighbors");
+  const neighborsList = clickedCountry.querySelector(".neighbors__list");
+
+  const html = ` 
+  <li class="neighbors__item">
+    <div class="neighbors__flag">
+      <img src="${flagIcon}">
+    </div>
+    <h5 class="neighbors__name">${cca3}</h5>
+  </li>`;
+  neighborsList.insertAdjacentHTML("beforeend", html);
+};
+
+//neigbor toggler
+const neighborToggler = (clickedCountry) => {
+  const neighbors = clickedCountry.querySelector(".neighbors");
+
+  clickedCountry.classList.toggle("active");
+  neighbors.classList.toggle("active");
+};
+
+// Neighbors handler
+const countryClickHandler = (e) => {
+  const neighborsList = e.currentTarget.querySelector(".neighbors__list");
+  const neighbors = e.currentTarget.querySelector(".neighbors");
+  const borders = neighbors.dataset["borders"].split(",");
+
+  if (neighborsList.childNodes.length) {
+    neighborToggler(e.currentTarget);
+  } else {
+    for (const neighbor of borders) {
+      console.log();
+      getNeighbor(neighbor, e.currentTarget);
+    }
+    neighborToggler(e.currentTarget);
+  }
 };
 
 //Clear list
 const clearList = () => {
+  searchedCountries = [];
   list.innerHTML = "";
   clearListBtn.style.display = "none";
 };
@@ -147,15 +210,31 @@ const handleForm = (e) => {
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
   const { "country-name": countryName } = formProps;
-  getCountry(countryName);
-  clearListBtn.style.display = "block";
-  e.target.reset();
+
+  const isExist = searchedCountries.some(
+    (country) => country === countryName.toLowerCase()
+  );
+  if (!isExist) {
+    formInput.setCustomValidity("");
+    searchedCountries.push(countryName);
+    getCountry(countryName);
+    clearListBtn.style.display = "block";
+    e.target.reset();
+  } else {
+    e.target.reset();
+    popUpToggler("Country already exists in list!");
+  }
 };
 
-//Main function
-const main = () => {};
+const popUpToggler = (message) => {
+  popUp.querySelector(".pop-up__heading").innerText = message;
+  popUp.classList.add("active");
+  setTimeout(() => {
+    popUp.classList.remove("active");
+    popUp.querySelector(".pop-up__heading").innerText = "";
+  }, 2000);
+};
 
 //Main event listeners
-document.addEventListener("DOMContentLoaded", main);
 form.addEventListener("submit", handleForm);
 clearListBtn.addEventListener("click", clearList);
